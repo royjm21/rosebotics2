ecd8de3442771e74079739784ae4464ed8950c4d
royjm <royjm@rose-hulman.edu>
2018-11-13 16:45:38 -0500

    finished initial gui, need to add color sensor and proximity sensor stuff, send mqtt message to gui to increment progress bar

--- src/Capstone_royjm_runs_on_robot.py
+++ src/Capstone_royjm_runs_on_robot.py
@@ -41,21 +41,19 @@ class RemoteControlEtc(object):
         self.robot = robot
 
     def go_forward(self, speed_string):
-        """makes robot move forward at a given speed"""
-        print('robot should move at speed = speed_string', speed_string)
-        robot = rb.Snatch3rRobot()
-        robot.drive_system.right_wheel.reset_degrees_spun()
-        robot.drive_system.left_wheel.reset_degrees_spun()
+        """makes the robot go forward at the given speed"""""
+        print('telling the robot to start moving at', speed_string)
+        speed = int(speed_string)
+        self.robot.drive_system.start_moving(speed, speed)
 
     def follow_path(self):
-        robot = rb.Snatch3rRobot
         while True:
-            x = robot.color_sensor.get_reflected_intensity()  # 100 in intensity is white, 1 through 4 is black
+            x = self.robot.color_sensor.get_reflected_intensity()  # 100 in intensity is white, 1 through 4 is black
             print(x)
             if x <= 10:
-                robot.drive_system.start_moving(left_wheel_duty_cycle_percent=45, right_wheel_duty_cycle_percent=45)
+                self.robot.drive_system.start_moving(left_wheel_duty_cycle_percent=45, right_wheel_duty_cycle_percent=45)
             if x >= 10:  # go counterclockwise in this case
-                robot.drive_system.start_moving(left_wheel_duty_cycle_percent=0, right_wheel_duty_cycle_percent=50)
+                self.robot.drive_system.start_moving(left_wheel_duty_cycle_percent=0, right_wheel_duty_cycle_percent=50)
 
 
 main()
